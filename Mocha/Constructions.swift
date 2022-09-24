84fae075562a4444d9f28f8ec6a6b855c77a0eac
deejHeath <heathdj@plu.edu>
2022-09-24 10:27:22 -0700

    nothing changed, but i can't convince Xcode

---
+++ Mocha/Constructions.swift
@@ -0,0 +1,20 @@
+//
+//  Constructions.swift
+//  Mocha
+//
+//  Created by Daniel Heath on 9/24/22.
+//
+
+import UIKit
+
+class Constructions: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
