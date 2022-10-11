f48d4653066e6cbadc66e29c389846c0891c56a4
deejHeath <heathdj@plu.edu>
2022-10-10 21:32:11 -0700

    A better AppIcon, though I'm still working on improving it.

--- Mocha/CreationViewController.swift
+++ Mocha/CreationViewController.swift
@@ -26,47 +26,4 @@ class CreationViewController: UIViewController {
         completionHandler?(sender.tag)
         dismiss(animated: true, completion: nil)
     }
-    func update(_ constructList: [Construction]) {
-//        linkedList = constructList
-//        constructs=[0,0,0]
-//        print(settingsButton)
-//        for object in linkedList {
-//            if object.type>0 && object.type<20 {
-//                constructs[0]+=1
-//            } else if object.type<0 {
-//                constructs[1]+=1
-//            } else if object.type==0 {
-//                constructs[2]+=1
-//            }
-//            for i in 0...13 {
-//                settingsButton[i].isEnabled=true
-//            }
-//            settingsButton[5].isEnabled=false       // get rid of this when segments enabled
-//            settingsButton[6].isEnabled=false       // get rid of this when rays enabled
-//            if constructs[0]<3 {
-//                settingsButton[13].isEnabled=false
-//            }
-//            if constructs[0]<2 {
-//                settingsButton[1].isEnabled=false
-//                settingsButton[5].isEnabled=false
-//                settingsButton[6].isEnabled=false
-//                settingsButton[7].isEnabled=false
-//                settingsButton[12].isEnabled=false
-//            }
-//            if constructs[1]<2 {
-//                settingsButton[10].isEnabled=false
-//                settingsButton[111].isEnabled=false
-//            }
-//            if constructs[1]<1 {
-//                settingsButton[8].isEnabled=false
-//                settingsButton[9].isEnabled=false
-//            }
-//            if constructs[2]<1 {
-//                settingsButton[4].isEnabled=false
-//            }
-//            if constructs[1]+constructs[2]<2 {
-//                settingsButton[3].isEnabled=false
-//            }
-//        }
-    }
 }
