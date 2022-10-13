322e388328138a0efe297b8f10caa3131d34fbaf
deejHeath <heathdj@plu.edu>
2022-10-13 15:41:03 -0700

    Now one button scales, rotates, and translates all.  It required adding UIGestureRecognizerDelegate to MainViewController's class description, as well as some methods.

--- Mocha/MainViewController.swift
+++ Mocha/MainViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var infoXLabel: UILabel!
@@ -10,14 +10,13 @@ class MainViewController: UIViewController {
     var clickedList: [Construction] = []
     var clickedIndex: [Int] = []
     let actionText=["Create or move POINTS", "Swipe between POINTS to create midpoint","Intersect 2 OBJECTS","Fold POINT over LINE","Invert POINT in CIRCLE", "Swipe between POINTS to create segment", "Swipe between POINTS to create ray","Swipe between POINTS to create line","Create line on POINT and ⊥ to LINE","Create line on POINT and || to LINE","Create bisector from 2 LINES","Fold from 2 POINTS to 2 LINES","Swipe between POINTS to create circle","Create 3 POINT circle"]
-    let measureText=["Measure distance between 2 POINTS","Measure angle from 3 POINTS","Measure area of triangle from 3 POINTS","Measure area of CIRCLE", "Measure sum of two MEASURES","Measure difference of 2 MEASURES","Measure product of 2 MEASURES","Measure ratio of 2 MEASURES","FIND sine of MEASURE","Find cosine of MEASURE.","Hide OBJECT","Show or hide label of OBJECT","Swipe to move everything","Pinch to scale everything"]
+    let measureText=["Measure distance between 2 POINTS","Measure angle from 3 POINTS","Measure area of triangle from 3 POINTS","Measure area of CIRCLE", "Measure sum of two MEASURES","Measure difference of 2 MEASURES","Measure product of 2 MEASURES","Measure ratio of 2 MEASURES","FIND sine of MEASURE","Find cosine of MEASURE.","Hide OBJECT","Show or hide label of OBJECT","Swipe/pinch to move/scale"]
     let makePoints=0, makeMidpoint=1, makeIntersections=2, foldPoints=3, invertPoints=4
     let makeSegments=5, makeRays=6, makeLines=7, makePerps=8, makeParallels=9
     let makeBisectors=10, useOrigamiSix=11, makeCircles=12, make3PTCircle=13
     let measureDistance=20, measureAngle=21, measureTriArea=22, measureCircArea=23
     let measureSum=24, measureDifference=25, measureProduct=26, measureRatio=27
-    let measureSine=28, measureCosine=29, hideObject=30, toggleLabel=31, translateAll=32
-    let scaleEverything=33
+    let measureSine=28, measureCosine=29, hideObject=30, toggleLabel=31, scaleEverything=32
     let POINT = 1, PTonLINE = 2, PTonCIRCLE = 3, MIDPOINT = 4
     let LINEintLINE = 5, FOLDedPT = 6, INVERTedPT=7
     let CIRCintCIRC0 = 8,CIRCintCIRC1 = 9, LINEintCIRC0 = 10, LINEintCIRC1 = 11
@@ -45,16 +44,57 @@ class MainViewController: UIViewController {
         canvas.translatesAutoresizingMaskIntoConstraints = false
         canvas.backgroundColor = .white
         canvas.isUserInteractionEnabled = true
-        canvas.isMultipleTouchEnabled = true // if we want to add dilation functionality this will need to be true
+        canvas.isMultipleTouchEnabled = true
         let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(didPinch(_:)))
+        let rotateGesture = UIRotationGestureRecognizer.init(target: self, action: #selector(didRotate(_:)))
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
+        pinchGesture.delegate=self
+        rotateGesture.delegate=self
+        panGesture.delegate=self
         canvas.addGestureRecognizer(pinchGesture)
+        canvas.addGestureRecognizer(rotateGesture)
+        canvas.addGestureRecognizer(panGesture)
         view.addSubview(canvas)
         NSLayoutConstraint.activate([canvas.centerXAnchor.constraint(equalTo: view.centerXAnchor),canvas.centerYAnchor.constraint(equalTo: view.centerYAnchor),canvas.widthAnchor.constraint(equalTo: view.widthAnchor),canvas.heightAnchor.constraint(equalToConstant: view.frame.height-200)])
+    }
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+            return true
+        }
+    @objc func didPan(_ recognizer : UIPanGestureRecognizer) {
+        if whatToDo==scaleEverything {
+            let translation = recognizer.translation(in: self.view)
+            for i in 0..<linkedList.count {
+                if linkedList[i].type>0 && linkedList[i].type<=PTonCIRCLE {
+                    update(object: linkedList[i], point: CGPoint(x: linkedList[i].coordinates.x+translation.x ,y: linkedList[i].coordinates.y+translation.y))
+                } else if linkedList[i].type<DISTANCE {
+                    update(object: linkedList[i], point: linkedList[i].coordinates)
+                }
+            }
+            canvas.update(constructions: linkedList, indices: clickedIndex)
+            canvas.setNeedsDisplay()
+            recognizer.setTranslation(CGPoint.zero, in: self.view)
+        }
+    }
+    @objc func didRotate(_ recognizer : UIRotationGestureRecognizer) {
+        if whatToDo==scaleEverything {
+            let C=cos(recognizer.rotation), S=sin(recognizer.rotation)
+            for i in 0..<linkedList.count {
+                if linkedList[i].type>0 && linkedList[i].type<=PTonCIRCLE {
+                    let X=linkedList[i].coordinates.x-canvas.frame.width/2.0
+                    let Y=linkedList[i].coordinates.y-canvas.frame.height/2.0
+                    update(object: linkedList[i], point: CGPoint(x: C*X-S*Y+canvas.frame.width/2.0 ,y: S*X+C*Y+canvas.frame.height/2.0))
+                } else if linkedList[i].type<DISTANCE {
+                    update(object: linkedList[i], point: linkedList[i].coordinates)
+                }
+            }
+            canvas.update(constructions: linkedList, indices: clickedIndex)
+            canvas.setNeedsDisplay()
+            recognizer.rotation = 0
+        }
     }
     @objc private func didPinch(_ gesture: UIPinchGestureRecognizer) {
         if gesture.state == .changed && whatToDo==scaleEverything {
             pinchScale=gesture.scale
-            print(pinchScale)
             gesture.scale=1.0
             for i in 0..<linkedList.count {
                 if linkedList[i].type>0 && linkedList[i].type<=PTonCIRCLE {
@@ -176,8 +216,6 @@ class MainViewController: UIViewController {
             if !activeConstruct {
                 potentialClick=nil
             }
-            break
-        case translateAll:
             break
         default:
             print("touchesBegan \(location)")
@@ -327,16 +365,6 @@ class MainViewController: UIViewController {
         case hideObject, toggleLabel:
             getRidOfActivesThatAreTooFar(location)
             getPointOrLineOrCircle(location)
-            break
-        case translateAll:
-            for i in 0..<linkedList.count {
-                if linkedList[i].type>0 && linkedList[i].type<=PTonCIRCLE {
-                    update(object: linkedList[i], point: CGPoint(x: linkedList[i].coordinates.x+location.x-firstTouch!.x,y: linkedList[i].coordinates.y+location.y-firstTouch!.y))
-                } else if linkedList[i].type<DISTANCE {
-                    update(object: linkedList[i], point: location)
-                }
-            }
-            firstTouch=location
             break
         default:
             print("touchesMoved: \(location)")
@@ -1034,8 +1062,6 @@ class MainViewController: UIViewController {
                 linkedList[clickedIndex[0]].showLabel = !linkedList[clickedIndex[0]].showLabel
                 clearAllPotentials()
             }
-            break
-        case translateAll:
             break
         default:
             print("touchesEnded: \(location)")
